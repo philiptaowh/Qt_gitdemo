d10d8a1671167ef72fcfc454935b4f472d0ac8ba
philiptaowh <philip.tao.wh@gmail.com>
2023-11-23 12:09:28 +0800

    second commit

--- widget.cpp
+++ widget.cpp
@@ -13,3 +13,21 @@ Widget::~Widget()
     delete ui;
 }
 
+
+void Widget::on_pushButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void Widget::on_pushButton_2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void Widget::on_pushButton_3_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
